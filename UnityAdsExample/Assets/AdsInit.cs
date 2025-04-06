1f0012aac457ed03ed14c054399234e8e9e3fb3a
MarsalekDesmotes <57544847+MarsalekDesmotes@users.noreply.github.com>
2025-04-06 15:18:25 +0300

    Sample Scene and Ads Init Script added.

--- UnityAdsExample/Assets/AdsInit.cs
+++ UnityAdsExample/Assets/AdsInit.cs
@@ -1,41 +1,122 @@
 using UnityEngine;
 using UnityEngine.Advertisements;
- 
-public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
+
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
 {
+    [Header("Game IDs")]
     [SerializeField] string _androidGameId;
     [SerializeField] string _iOSGameId;
     [SerializeField] bool _testMode = true;
+
+    [Header("Ad Unit IDs")]
+    [SerializeField] string _androidInterstitialAdUnitId = "Interstitial_Android";
+    [SerializeField] string _androidRewardedAdUnitId = "Rewarded_Android";
+    [SerializeField] string _androidBannerAdUnitId = "Banner_Android";
+
+    [SerializeField] string _iOSInterstitialAdUnitId = "Interstitial_iOS";
+    [SerializeField] string _iOSRewardedAdUnitId = "Rewarded_iOS";
+    [SerializeField] string _iOSBannerAdUnitId = "Banner_iOS";
+
     private string _gameId;
- 
+    private string _interstitialAdUnitId;
+    private string _rewardedAdUnitId;
+    private string _bannerAdUnitId;
+
+    private System.Action _onRewardedComplete;
+
     void Awake()
     {
         InitializeAds();
     }
- 
+
     public void InitializeAds()
     {
-    #if UNITY_IOS
-            _gameId = _iOSGameId;
-    #elif UNITY_ANDROID
-            _gameId = _androidGameId;
-    #elif UNITY_EDITOR
-            _gameId = _androidGameId; //Only for testing the functionality in the Editor
-    #endif
-        if (!Advertisement.isInitialized && Advertisement.isSupported)
+#if UNITY_IOS
+        _gameId = _iOSGameId;
+        _interstitialAdUnitId = _iOSInterstitialAdUnitId;
+        _rewardedAdUnitId = _iOSRewardedAdUnitId;
+        _bannerAdUnitId = _iOSBannerAdUnitId;
+#else
+        _gameId = _androidGameId;
+        _interstitialAdUnitId = _androidInterstitialAdUnitId;
+        _rewardedAdUnitId = _androidRewardedAdUnitId;
+        _bannerAdUnitId = _androidBannerAdUnitId;
+#endif
+
+        Advertisement.Initialize(_gameId, _testMode, this);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialized.");
+        Advertisement.Load(_interstitialAdUnitId, this);
+        Advertisement.Load(_rewardedAdUnitId, this);
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Load(_bannerAdUnitId);
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.LogError($"Unity Ads Initialization Failed: {error} - {message}");
+    }
+
+    // ---------- Interstitial ----------
+    public void ShowInterstitialAd()
+    {
+        Advertisement.Show(_interstitialAdUnitId, this);
+    }
+
+    // ---------- Rewarded ----------
+    public void ShowRewardedAd(System.Action onRewarded)
+    {
+        _onRewardedComplete = onRewarded;
+        Advertisement.Show(_rewardedAdUnitId, this);
+    }
+
+    // ---------- Banner ----------
+    public void ShowBanner()
+    {
+        Advertisement.Banner.Show(_bannerAdUnitId);
+    }
+
+    public void HideBanner()
+    {
+        Advertisement.Banner.Hide(false);
+    }
+
+    // ---------- Load Listener ----------
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        Debug.Log($"Ad Loaded: {placementId}");
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        Debug.LogError($"Failed to Load Ad: {placementId} - {error} - {message}");
+    }
+
+    // ---------- Show Listener ----------
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        if (placementId == _rewardedAdUnitId && showCompletionState == UnityAdsShowCompletionState.COMPLETED)
         {
-            Advertisement.Initialize(_gameId, _testMode, this);
+            Debug.Log("Rewarded ad watched completely. Grant reward.");
+            _onRewardedComplete?.Invoke();
         }
     }
 
- 
-    public void OnInitializationComplete()
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
     {
-        Debug.Log("Unity Ads initialization complete.");
+        Debug.LogError($"Ad Show Failed: {placementId} - {error} - {message}");
     }
- 
-    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+
+    public void OnUnityAdsShowStart(string placementId)
     {
-        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+        Debug.Log($"Ad Show Started: {placementId}");
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        Debug.Log($"Ad Clicked: {placementId}");
     }
 }
